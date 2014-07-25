0cad49ed62563a3b5f1e0764d620d45b84a02bb9
Carson Tam <carsontam@yahoo.com>
2014-07-25 14:23:35 -0700

    Added remove files to modify-code.sh.

--- modify-code.sh
+++ modify-code.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# Make sure we are in moodle-internallogin directory
+if [ $(basename `pwd`) != moodle-internallogin ]; then
+    echo "WARNING: Make sure you're in the right directory (i.e. moodle-internallogin) since this script will remove files."
+    exit 0
+fi
+
+rm *.html *.php
+cp -Rp ../moodle/login/* .
+
 for f in *.{php,html}; do sed -i 's|/login/|/internallogin/|g' $f; done
 
 for f in *.{php,html}; do sed -i "s|(!empty(\$CFG->alternateloginurl))|(false /* disable alternate login */ and !empty(\$CFG->alternateloginurl))|g" $f; done
