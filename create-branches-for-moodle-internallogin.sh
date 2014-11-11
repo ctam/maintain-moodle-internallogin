e41605222abb5e9693b76d9559156e3c1d21b563
Carson Tam <carson.tam@ucsf.edu>
2014-11-11 00:32:24 -0800

    Updated to create new branch and check in new code.

--- create-branches-for-moodle-internallogin.sh
+++ create-branches-for-moodle-internallogin.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 set -e
 
-for ver in {4..7}; do
+for ver in {4..8}; do
     version=v2.${ver}.0
     branch=MOODLE_2${ver}_STABLE
 
     cd moodle
     git checkout $version
     cd ../moodle-internallogin/
-    git checkout -b $branch
-    cp -Rp ../moodle/login/* .
-    git add .
-    git commit -am"Initial original copy of login directory from Moodle $version."
-    git push origin $branch
-    git checkout master
+    if [ `git branch | grep $branch` ]; then
+        echo "$branch already exists, skipping."
+    else
+        # check in the current version to master
+        git checkout master
+        cp -Rp ../moodle/login/* .
+        git add .
+        git commit -am"Initial original copy of login directory from Moodle $version."
+        git push origin master
+
+        git checkout -b $branch
+        git push origin $branch
+        git branch -u origin/$branch
+
+        # apply internallogin changes
+        ../modify-code.sh --commit
+    fi
     cd ..
 done
--- update-branch-to-tag.sh
+++ update-branch-to-tag.sh
@@ -53,12 +53,7 @@ fi
 git commit -am"Initial original copy of login directory from Moodle ${tag}."
 
 # Start modifying the code for internal login
-for f in *.{php,html}; do sed -i '' 's|/login/|/internallogin/|g' $f; done
-git commit -am"Replaced URLs that contain '/login/' with '/internallogin/' by running: for f in *.{php,html}; do sed -i 's|/login/|/internallogin/|g' \$f; done"
-
-for f in *.{php,html}; do sed -i '' "s|(!empty(\$CFG->alternateloginurl))|(false /* disable alternate login */ and !empty(\$CFG->alternateloginurl))|g" $f; done
-for f in *.{php,html}; do sed -i '' "s|= get_login_url();|= str_replace('/login/', '/internallogin/', get_login_url());|g" $f; done
-git commit -am"Stopped alternateloginurl and corrected url returned by get_login_url()."
+../modify-code.sh --commit
 
 # Merge to branch
 git checkout ${branch}
